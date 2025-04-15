3d201e2a9d25f5c7bc5ced82c39401370749fce0
arcevit <hakkisuman@gmail.com>
2025-04-15 12:06:18 +0200

    add dependencies and initilize app

---
+++ src/main/AppProvider.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider as ChakraProvider } from "../chakra-ui-components/provider";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
+
+const AppProviders: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  return (
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ChakraProvider>
+  );
+};
+
+export default AppProviders;
